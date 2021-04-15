dc9b3ee4da9a036b49c16a3233d68fa9bdb1c914
Sean Mcguigan <sean@clim8invest.com>
2021-04-15 17:22:00 +0100

    bug fix

--- terraform/development/eks_vpc/outputs.tf
+++ terraform/development/eks_vpc/outputs.tf
@@ -1,23 +1,23 @@
-output public_subnets {
+output "public_subnets" {
   value = module.eks-vpc.public_subnets
 }
 
-output private_subnets {
+output "private_subnets" {
   value = module.eks-vpc.private_subnets
 }
 
-output all_subnets {
+output "all_subnets" {
   value = concat(module.eks-vpc.private_subnets, module.eks-vpc.public_subnets)
 }
 
-output nat_public_ips {
+output "nat_public_ips" {
   value = module.eks-vpc.nat_public_ips
 }
 
-output vpc_id {
+output "vpc_id" {
   value = module.eks-vpc.vpc_id
 }
 
-output vpc_cidr_block {
+output "vpc_cidr_block" {
   value = module.eks-vpc.vpc_cidr_block
 }
